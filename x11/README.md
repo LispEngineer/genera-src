27c75fb8580dcc2a53f335a19d7eb1900434fe76
Douglas Fields <symbolics@lisp.engineer>
2016-07-16 15:26:13 -0400

    Making progress getting X Keyboard Remapped

--- x11/README.md
+++ x11/README.md
@@ -16,6 +16,71 @@ Genera seems to read the xmodmap once when the screen is started, to get
 all the keynums and keysyms. If you change the xmodmap, it doesn't seem to
 pick up on that later.
 
+* `(x-screen::default-x-screen)` returns the default X-Windows screen. It seems to be the current one, as it will return a different value for each X-Windows connection, e.g.:
+  * `#<X-REAL-SCREEN X Screen INTERNET|192.168.1.106:0.0 0 (Genera on BERYLLIUM) 1004206265 exposed>
+
+You can get a display by opening it, but I don't see how to get the current one
+out of Flavors yet.
+
+* `(xlib:open-display "INTERNET|192.168.1.106")`
+  * This will give an error: `Error: INTERNET|192.168.1.106 does not support X-WINDOW-SYSTEM service.`
+  * Simply respond `s-A: Use protocol X-WINDOW-SYSTEM on medium TCP.`
+* `(xlib:display-vendor-name \*)
+  * `"The X.Org Foundation"`
+
+You can find out a lot of information with the various functions. Search for
+`def-clx-class (display` in the `clx.lisp` file to see what all the parts of the
+display structure are.
+
+* `(xlib:display-vendor-name ...)`
+* `(xlib:display-release-number ...)`
+* `(xlib:display-host ...)`
+* `(xlib:display-default-screen ...)`
+* `(xlib:display-vendor ...)` - returns multiple values, `vendor-name` and `release-number`
+* `(xlib::display-keysym-mapping ...)` - `nil`?
+* `(xlib::display-modifier-mapping ...)` - `nil`?
+* `(xlib::display-keysym-tanslation ...)` - `nil`?
+* `(xlib:display-min-keycode ...)`
+* `(xlib:display-max-keycode ...)`
+* `(xlib:display-roots ...)`
+* `(xlib:display-xdefaults ...)` - `nil`?
+
+So the `keyboard-signature` seems to be a way for Genera to automatically figure
+out which keyboard is in use at the destination X screen and set the mappings
+appropriately! As it turns out, the Xquarts 2.7.10_beta2 on macOS 10.11.5 returns
+these relevant data about its screen:
+
+* `min-keycode`: 8 - which is mapped to the `keycode-offset` when creating a signature
+* `vendor-name`: `The X.Org Foundation`
+* `vendor-version`: 118030000
+
+Indeed, I created a test keyboard signature starting with the below:
+
+```lisp
+(define-keyboard-signature :xquartz-87-tenkeyless
+                           (:keycode-offset 8
+                            :vendor-name "The X.Org Foundation"
+                            :vendor-version 11803000)
+```
+
+When I started a new X screen (`Start X Screen INTERNET|192.168.1.106 :Reuse No`),
+and immediately checked the maping (`Show X Keyboard Mapping`), it responded
+as I would hope:
+
+```
+The keyboard layout type is :XQUARTZ-87-TENKEYLESS.
+```
+
+Now, the next matter turns to the `define-keyboard-mapping`
+to try to set the key mapping reasonably usably.
+
+```lisp
+(define-keyboard-mapping :xquartz-87-tenkeyless ()
+```
+
+
+ 
+
 # `dpf-x-kb-2.lisp`
 
 Testing code for above
--- x11/dpf-x-kb-2.lisp
+++ x11/dpf-x-kb-2.lisp
@@ -221,3 +221,164 @@
   ; Now the delete key can do something else (but what?)
   (:delete #\back-space)
 )
+
+
+(define-keyboard-signature :xquartz-87-tenkeyless
+                           (:keycode-offset 8
+                            :vendor-name "The X.Org Foundation"
+                            :vendor-version 11803000)
+  (8  :latin-small-letter-a :latin-capital-letter-a) ; Checked
+  (9  :latin-small-letter-s :latin-capital-letter-s) ; Checked
+  (10 :latin-small-letter-d :latin-capital-letter-d) ; Checked
+  (11 :latin-small-letter-f :latin-capital-letter-f) ; Checked
+  (12 :latin-small-letter-h :latin-capital-letter-h) ; Checked
+  (13 :latin-small-letter-g :latin-capital-letter-g) ; Checked
+  (14 :latin-small-letter-z :latin-capital-letter-z) ; Checked
+  (15 :latin-small-letter-x :latin-capital-letter-x) ; Checked
+  (16 :latin-small-letter-c :latin-capital-letter-c) ; Checked
+  (17 :latin-small-letter-v :latin-capital-letter-v) ; Checked
+  (19 :latin-small-letter-b :latin-capital-letter-b) ; Checked
+  (20 :latin-small-letter-q :latin-capital-letter-q) ; Checked
+  (21 :latin-small-letter-w :latin-capital-letter-w) ; Checked
+  (22 :latin-small-letter-e :latin-capital-letter-e) ; Checked
+  (23 :latin-small-letter-r :latin-capital-letter-r) ; Checked
+  (24 :latin-small-letter-y :latin-capital-letter-y) ; Checked
+  (25 :latin-small-letter-t :latin-capital-letter-t) ; Checked
+  (26 :digit-one            :exclamation-point) ; Checked
+  (27 :digit-two            :commercial-at) ; Checked
+  (28 :digit-three          :number-sign) ; Checked
+  (29 :digit-four           :dollar-sign) ; Checked
+  (30 :digit-six            :circumflex-accent) ; Checked
+  (31 :digit-five           :percent-sign) ; Checked
+  (32 :equals-sign          :plus-sign) ; Checked
+  (33 :digit-nine           :left-parenthesis) ; Checked
+  (34 :digit-seven          :ampersand) ; Checked
+  (35 :hyphen               :low-line) ; Checked
+  (36 :digit-eight          :asterisk) ; Checked
+  (37 :digit-zero           :right-parenthesis) ; Checked
+  (38 :right-square-bracket :right-curly-bracket) ; Checked
+  (39 :latin-small-letter-o :latin-capital-letter-o) ; Checked
+  (40 :latin-small-letter-u :latin-capital-letter-u) ; Checked
+  (41 :left-square-bracket  :left-curly-bracket) ; Checked
+  (42 :latin-small-letter-i :latin-capital-letter-i) ; Checked
+  (43 :latin-small-letter-p :latin-capital-letter-p) ; Checked
+  (44 :return               :return) ; Checked
+  (45 :latin-small-letter-l :latin-capital-letter-l) ; Checked
+  (46 :latin-small-letter-j :latin-capital-letter-j) ; Checked
+  (47 :apostrophe           :quotation-mark) ; Checked
+  (48 :latin-small-letter-k :latin-capital-letter-k) ; Checked
+  (49 :semicolon            :colon) ; Checked
+  (50 :reverse-solidus      :vertical-line) ; Checked
+  (51 :comma                :less-than-sign) ; Checked
+  (52 :solidus              :question-mark) ; Checked
+  (53 :latin-small-letter-n :latin-capital-letter-n) ; Checked
+  (54 :latin-small-letter-m :latin-capital-letter-m) ; Checked
+  (55 :full-stop            :greater-than-sign) ; Checked
+  (56 :tab                  :tab) ; Checked
+  (57 :space                :space) ; Checked
+  (58 :grave-accent         :tilde) ; Checked
+  (59 :backspace            :backspace) ; DELETE (i.e., rubout) - the key above the backslash
+
+  ; We want to remap escape to select, but have no practical way of doing this
+  ; using the mapping below. What if we just tell it it's select in the first place?
+  (61 :escape               :escape) ; Checked
+; (61 :select :select) ; ESCAPE KEY
+; (61 :f35 :f35) ; ESCAPE KEY
+
+  ; Modifiers ----------------
+  ; Xquartz can make alt/option send Alt_L or Mode_switch depending on 
+  ; setting of "Option keys send Alt_L and Alt_R"
+  (63 :left-meta            :left-meta) ; LEFT COMMAND (left ALT on Windows)
+  (64 :left-shift           :left-shift) ; Checked
+  (65 :caps-lock            :caps-lock) ; Sends two keycode 65 events in a row
+  (66 :left-alt             :left-alt) ; This is LEFT alt/option (left WIN on Windows) -
+  (67 :left-control         :left-control) ; Checked
+
+  (68 :right-shift          :right-shift)
+  (69 :right-alt            :right-alt) ; RIGHT alt/option (right WIN on Windows) - ADDED
+  (70 :right-control        :right-control) ; RIGHT CTRL - ADDED 
+  (71 :right-meta           :right-meta) ; RIGHT COMMAND (ALT on Windows) - ADDED
+  ; FIXME: Add right MENU key - key code 118
+
+  (104 :f5                  :f5) ; Checked
+  (105 :f6                  :f6) ; Checked
+  (106 :f7                  :f7) ; Checked
+  (107 :f3                  :f3) ; Checked
+  (108 :f8                  :f8) ; Checked
+  (109 :f9                  :f9) ; Checked
+  (111 :f11                 :f11) ; Checked
+  (113 :f13                 :print) ; F13 is PRINT SCREEN on Windows
+  (115 :f14                 :pause) ; F14 is SCROLL LOCK on Windows
+  (117 :f10                 :f10) ; Checked
+; (118 :menu :menu) ; Right menu key
+  (119 :f12                 :f12) ; Checked
+  (121 :f15                 :pause) ; F15 is PAUSE on Windows
+  (122 :help                :insert) ; HELP is INSERT on Windows
+  (123 :home                :home) ; Checked
+  (124 :prior               :prior) ; PAGE UP
+  (125 :delete              :delete) ; DELETE FORWARD
+  (126 :f4                  :f4) ; Checked
+  (127 :end                 :end) ; Checked
+  (128 :f2                  :f2) ; Checked
+  (129 :next                :next) ; PAGE DOWN
+  (130 :f1                  :f1) ; Checked
+  (131 :left                :left) ; Checked
+  (132 :right               :right) ; Checked
+  (133 :down                :down) ; Checked
+  (134 :up                  :up) ; Checked
+
+  ; The highest keycode we will natively have seems to be 134.
+  ; Keycodes after this seem to be related to OPTION-X and OPTION-SHIFT-X
+  ; keys, which I cannot get X to generate. However, definitely see the
+  ; apple-extended keyboard for more details.
+)
+
+; Intended mapping of bottom row:
+; Control Super Meta   SPACE   Meta Control Symbol Hyper
+
+(define-keyboard-mapping :xquartz-87-tenkeyless () ; (:leds ((1 :mode-lock) ; scroll lock?
+                                               ;     (2 :caps-lock)
+                                               ;     (3 :num-lock)))
+  ; #\Select and the others are special Genera-defined character names
+  (:f1  #\Escape #\Square)
+  ; :escape doesn't seem to allow us to remap...
+  (:escape #\Select #\Select)
+  (#\Escape #\Select)
+  (:f35 #\Select)
+  
+  ; Set the "delete" key to actaully delete backwards, i.e., rubout
+  (:backspace #\Rubout)
+  ; Now the delete key can do something else (but what?)
+  (:delete #\back-space)
+
+  (:f2  #\Network #\Circle)
+  (:f3  #\Function #\Triangle)
+  (:f4  #\Suspend (:mode-lock :locking t))
+  (:f5  #\Resume)
+  (:f6  #\Abort)
+  (:f7  :left-super)
+  (:f8  :left-hyper)
+  (:f9  #\Scroll #\Page)
+  (:f10 #\Clear-Input #\Refresh)
+  (:f11 #\Complete #\End)
+  (:f12 #\Help)
+
+  (:home     #\Keyboard:Home)
+  (:up       #\Keyboard:Up)
+  (:left     #\Keyboard:Left)
+  (:right    #\Keyboard:Right)
+  (:down     #\Keyboard:Down)
+  (:print    #\Keyboard:Print)
+  (:undo     #\Keyboard:Undo)
+;  (:redo     #\Keyboard:Redo)
+  (:find     #\Keyboard:Find)
+  (:next     #\Scroll)
+  (:prior    #\Keyboard:Back-Scroll)
+
+  (:break    #\Suspend)
+
+  (:num-lock (:num-lock :locking t))
+
+  (:left-alt :left-super)
+  (:right-alt :right-symbol)
+)
