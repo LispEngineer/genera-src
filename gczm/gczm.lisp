a42260178935dde70560cb8381c2a182fe891b39
Douglas Fields <symbolics@lisp.engineer>
2016-09-08 04:32:25 -0400

    Our interactor works with strings (mostly) but doesn't do accelerators nor menu handling

--- gczm/gczm.lisp
+++ gczm/gczm.lisp
@@ -56,16 +56,10 @@
 
 ; Functions so we can see what's going on afterwards or in the Lisp Listener
 (defparameter *log* ())
+(setq *log* nil) ; Clear the log if we're recompiling
 (defun addlog (message)
   (setf *log* (cons message *log*)))
 
-; Custom command parser
-; Just accept a string, and return the "Got an input" command
-(defun gczm-cl-command-parser (command-table stream)
-  (declare (ignore command-table stream))
-  (let ((result (accept 'string)))
-    (addlog (list "Got" result))
-    (list 'com-input result)))
 
 ; Main application frame
 ; TODO: Figure out how to send initial output to the interactor prior to
@@ -74,18 +68,20 @@
   ((z-machine :initform nil))
 
   ; Instead of a custom top-level, let's just have a custom read-frame-command
-  ;(:top-level (clim:default-frame-top-level :prompt "> "
+  (:top-level (clim:default-frame-top-level :prompt "> "))
   ;		:command-parser gczm-cl-command-parser))
 
   (:menu-bar nil) ; disable default menu bar and show it in pane explicitly
 
   (:panes
+    (title     :title)
+    #-Genera (pointerd  :pointer-documentation) ; Provided by Genera
     (commands  :command-menu) ; This is supplied automatically unless :menu-bar nil
     (display   :interactor
-	       ; :text-style '(:fix :bold :very-large)
                :scroll-bars :vertical
                :initial-cursor-visibility :on)
     (statusbar :application
+	       :max-height 20 :height 20 ; Calculate this from text size
                :display-function 'draw-the-statusbar
                ; TODO: Set the height to one line of characters - clim:text-size,
 	       ;       clim:stream-line-height
@@ -94,11 +90,12 @@
 
   ; Default command table will be named gc-z-machine-command-table
   ; Commands are symbols, conventionally starting with com-
-  ; (:command-table (gc-z-machine :inherit-from (clim:accept-values-pane)))
+  ; Enabling the accept-values-pane doesn't make "accept" work with the menu
+  (:command-table (gc-z-machine :inherit-from (clim:accept-values-pane)))
 
   (:layouts
     (main 
-      (vertically () commands display statusbar))))
+      (vertically () title commands display statusbar #-Genera pointerd))))
 
 ; Enable Keystroke Accelerators (hotkeys) - per Genera CLIM 2.0 Docs
 ; clim:read-command-using-keystrokes could have overrides for the
@@ -108,48 +105,53 @@
     (clim:with-command-table-keystrokes (keystrokes command-table)
       (clim:read-command-using-keystrokes command-table keystrokes))))
 
-; Custom command reader which accepts any string
+; Custom command reader which accepts any string.
+; Note that this seems to prevent clicking on the menu bar and
+; command accelerators from working... Although, the accelerators
+; are definitely causing the accept to terminate.
 (defmethod read-frame-command ((frame gc-z-machine) &key (stream *standard-input*))
   "Specialized for GCZM, just reads a string and returns it with com-input"
   (multiple-value-bind (astring thetype)
-      ; Accept returns the object and the type
+      ; Accept returns the object and the type.
+      ; It does some parsing on the input, for example, removing surrounding
+      ; doublequotes.
+      ; TODO: To fix this, we probably need to make our own presentation type
+      ; that's exactly a string without any parsing.
       (accept 'string :stream stream :prompt nil
                       :default "" :default-type 'string)
-    (declare (ignore thetype))
+    ; (declare (ignore thetype))
+    (addlog (list "read-frame-command" astring thetype))
     ; Now that we have astring & thetype, return our command
     (list 'com-input astring)))   
 
-; We need to figure out how to just append stuff to this each time
-; rather than constantly redrawing the whole thing.
-(defmethod draw-the-display ((application gc-z-machine) stream)
-  (fresh-line stream)
-  (write-string "Genera CLIM Z-Machine Interpreter v0.01" stream)
-  (dolist (ll (reverse *log*))
-    (fresh-line stream)
-    (format stream "~A" ll)))
-
 (defmethod draw-the-statusbar ((application gc-z-machine) stream)
   (write-string "West of House          Turn 3         Score 73" stream))
+
+; Commands ---------------------------------------------------------------
 
 (define-gc-z-machine-command (com-exit :menu t       ; Show in menu
                                        :keystroke (:q :meta)
                                        :name "Exit") ; Type "Exit" to quit application
                              ()
+  (addlog (list "Called com-exit"))
   (frame-exit *application-frame*))
 
 ; If we get input from the command line processor, this is it...
 (define-gc-z-machine-command (com-input) ((astring 'string))
   ; First, write to our interactor
-  (fresh-line *standard-output*)
-  (write-string "Got: " *standard-output*)
-  (write-string astring *standard-output*)
-  (fresh-line *standard-output*)
+  (fresh-line *standard-input*)
+  (write-string "Got: " *standard-input*)
+  (write-string astring *standard-input*)
+  (fresh-line *standard-input*)
   ; Then store this permanently for debugging
   (addlog (list "Called com-input with" astring))
   astring)
 
+; Testing -------------------------------------------------------------------
+
 #||
 () ; Necessary so we can do c-sh-E to execute the below
+; c-m-ABORT to get out if it gets stuck (either from the CLIM window or Zmacs)
 (run-frame-top-level 
   (setq gczm1 (make-application-frame 'gc-z-machine
                :left 100 :right 600 :top 100 :bottom 500)))
