6404dbf30bcc555034a6ac3d31da943086f1be4b
Douglas Fields <symbolics@lisp.engineer>
2016-09-14 20:56:59 -0400

    Make the conditions program work in Genera, but it has a lot of Genera-specific characters that will be removed in next commit

--- gczm/cond.lisp
+++ gczm/cond.lisp
@@ -1,24 +1,58 @@
 ;;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; Package: CONDTEST; Base: 10; Lowercase: Yes -*-
 
 ;; A comparison of condition handling in modern SBCL 1.3.9 vs
-;; Genera 8.3+
+;; Genera 8.3
 
 ;; Douglas P. Fields, Jr. - https://symbolics.lisp.engineer/
+
+;; NOTE: On Genera, before loading this file, create the package.
+;; Allowing the editor to create the package doesn't make it correctly.
+;; At the Genera Lisp Listener:
+;;  Set Lisp Context (a lisp syntax name [default Common-Lisp]) ANSI-Common-Lisp
+;;  (defpackage condtest (:use cl conditions))
+
 
 #+SBCL
 (defpackage :condtest
   (:use :common-lisp))
 
+#+Genera
+(defpackage :condtest
+  (:use future-common-lisp conditions))
+
 (in-package :condtest)
 
 
-;; SBCL -------------------------------------------------------------------
-
-
-;; Code from http://c2.com/cgi/wiki?CommonLispConditionSystem
+;; Do a little test of conditions now, using
+;; code originally from http://c2.com/cgi/wiki?CommonLispConditionSystem
 ;; (but fixed, they had the parentheses wrong)
+
+
+#+SBCL
 (define-condition on-zero-denominator (error)
   ((message :initarg :message :reader message)))
+
+;; Genera requires the :report below. If it's absent, then
+;; we get the following error:
+;; D,#TD1PsT[Begin using 006 escapes](1 0 (NIL 0) (:FIX :BOLD :NORMAL) "CPTFONTCB")Error: Error in flavor CONDTEST::ON-ZERO-DENOMINATOR
+0;;        1Missing method DBG:REPORT (required by CONDITION)
+0#+Genera
+(define-condition on-zero-denominator (error)
+  ((message :initarg :message :reader message))
+  (:report (lambda (condition stream)
+	     (format stream "On-zero-denominator: ~A" (message condition)))))
+
+#| 
+;; From Genera Document Examiner DEFINE-CONDITION macro docs
+;; Note that the original had unbalanced parens; fixed below.
+(2 0 (NIL 0) (SAGE:SANS-SERIF-BODY SAGE::TYPEWRITER :NORMAL) "CPTFONT")(DEFINE-CONDITION MACHINE-ERROR (ERROR) 
+0  (2(MACHINE-NAME
+0    2:INIT-ARG :MACHINE-NAME
+0    2:READER   MACHINE-ERROR-MACHINE-NAME))
+  (:REPORT (LAMBDA (CONDITION STREAM)
+0	     2(FORMAT STREAM "There is a problem with ~A."
+0		     2(MACHINE-ERROR-MACHINE-NAME CONDITION)))))
+0|#
 
 (defun high-level-code ()
   (handler-bind
